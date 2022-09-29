d3287841b47b6bdd5cad34ba4a9ff4849438fbd8
Yevhen Arkhipov <arkhipovyevhen@gmail.com>
2022-09-29 22:59:15 +0300

    fix

--- src/js/components/refs.js
+++ src/js/components/refs.js
@@ -1,7 +1,15 @@
-export const refs = {
-  gallery: document.querySelector('.gallery'),
-  watchedBtn: document.querySelector('library-buttons-button watched'),
-  queueBtn: document.querySelector('library-buttons-button queue'),
-  addToWatchedBtn: document.querySelector('add-to-watched'),
-  addToQueueBtn: document.querySelector('add-to-queue'),
+const refs = {
+  // gallery: document.querySelector('.gallery'),
+  watchedBtn: document.querySelector('.watched'),
+  queueBtn: document.querySelector('.queue'),
+  // addToWatchedBtn: document.querySelector('.add-to-watched'),
+  // addToQueueBtn: document.querySelector('add-to-queue'),
+  container: document.querySelector('.container'),
 };
+
+// console.log(refs.gallery);
+console.log(refs.watchedBtn);
+console.log(refs.queueBtn);
+// console.log(refs.addToWatchedBtn);
+// console.log(refs.addToQueueBtn);
+console.log(refs.container);
