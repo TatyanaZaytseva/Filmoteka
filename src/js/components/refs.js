6ef0dce9c803b1ad670dbd925649f5131814f315
Yevhen Arkhipov <arkhipovyevhen@gmail.com>
2022-10-01 13:24:14 +0300

    switch-theme

--- src/js/components/refs.js
+++ src/js/components/refs.js
@@ -1,7 +1,10 @@
 export const refs = {
+  body: document.querySelector('body'),
   container: document.querySelector('.container'),
   library: document.querySelector('.library'),
   header: document.querySelector('header'),
+  tuiPage: document.querySelector('.tui-pagination'),
+  footer: document.querySelector('.footer'),
   scrollBtn: document.querySelector('.scroll-up'),
   // gallery: document.querySelector('.gallery'),
   watchedBtn: document.querySelector('.watched'),
@@ -9,6 +12,8 @@ export const refs = {
   modal: document.querySelector('.modal-overlay'),
   addToWatchedBtn: document.querySelector('.add-to-watched'),
   movieList: document.querySelector('.movie-list'),
+  movieModal: document.querySelector('.movie-modal'),
   movieModalWrapper: document.querySelector('.movie-modal-wrapper-js'),
   movieModalClose: document.querySelector('.movie-modal-close-js'),
+  toggle: document.querySelector('.theme-switch__toggle'),
 };
