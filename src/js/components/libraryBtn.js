2ffa435f4b35fa4fb52398c25b2b4f8066a1e897
Alla-Goptsii <goptsy.a@gmail.com>
2022-10-01 15:03:33 +0300

    clear commits

--- src/js/components/libraryBtn.js
+++ src/js/components/libraryBtn.js
@@ -5,17 +5,21 @@ import { renderMovieDetails } from '../templates/movie-details.js';
 import { renderCard } from '../templates/movie-card.js';
 // import { IMAGE_URL } from '../globals';
 import { getGenres } from '../http/getGenres';
+import { renderMovies } from './movie-list';
 
 console.log(refs.watchedBtn);
-// console.log(STORAGE_KEY);
-// if (pageName.includes('MY')) {
 
-//   if (watchedFilms) {
-//     refs.library = watchedFilms;
-//   }
 // if (document.title === 'Filmoteka') {
 //   refs.watchedBtn === disabled;
 // }
+// function isLibrary() {
+//   const pageName = document.location.pathname;
+//     if (pageName.includes('library')) {
+//     }
+//   else {
+//  refs.watchedBtn.removeEventListener('click');
+//   refs.queueBtn.removeEventListener('click');}
+
 console.log(document.title);
 // if (document.title === 'My Library') {
 refs.watchedBtn.addEventListener('click', onClickBtnWatched);
@@ -23,51 +27,30 @@ refs.queueBtn.addEventListener('click', onClickBtnQueue);
 // }
 console.log(refs.library);
 
-function onClickBtnWatched(movies) {
+function onClickBtnWatched() {
   console.log('click', 'watched');
-  //   console.log(localStorage.getItem(STORAGE_KEY_WATCHED));
 
   const watchedFilms = JSON.parse(localStorage.getItem(STORAGE_KEY_WATCHED));
-  // let result = {};
   //   renderMovieDetails();
   if (watchedFilms) {
     console.log(watchedFilms);
-    // refs.library = watchedFilms;
-    // return result;
     // renderCard();
+    onRenderFilmLibrary();
     refs.library.innerHTML = watchedFilms;
   }
 }
 
 function onClickBtnQueue() {
   console.log('click', 'queue');
+  const queueFilms = JSON.parse(localStorage.getItem(STORAGE_KEY_QUEUE));
+  if (queueFilms) {
+    console.log(queueFilms);
+
+    // renderCard();
+    refs.library.innerHTML = queueFilms;
+  }
 }
 
-// function onRenderFilmLibrary() {
-//   renderMovies();
-// }
-//   let genres = Object.keys(localStorage).map(id => {
-//     return genres.find(genre => genre.id === id).name;
-//   });
-//   STORAGE_KEY_WATCHED.map(({ poster_path, title }) => {
-//     return `<li class="movie-card">
-//         < article class="movie-card__article" >
-//         <a class="movie-card__link" href="">
-//           <img width="440" height="660" class="movie-card__img" src="${
-//             STORAGE_KEY_WATCHED.poster_path
-//           }">
-//         </a>
-//         <div class="movie-card__header">
-//           <a href="">
-//             <h2 class="movie-card__title">${STORAGE_KEY_WATCHED.title}</h2>
-//           </a>
-//           <div class="movie-card__info">
-//             ${valuesId.join(
-//               ', '
-//             )} | ${STORAGE_KEY_WATCHED.release_date.substring(0, 4)}
-//           </div>
-//         </div>
-//       </>
-//     </li>`;
-//   }).join('');
-// }
+function onRenderFilmLibrary() {
+  renderMovies();
+}
