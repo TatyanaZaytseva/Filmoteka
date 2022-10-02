1d15ceca76f346ba346dc89259bb13337b5e5d83
Yushkov Vitaliy <98348805+Yushkov88@users.noreply.github.com>
2022-10-02 14:18:14 +0300

    fix moviecard

--- src/js/templates/movie-card.js
+++ src/js/templates/movie-card.js
@@ -9,12 +9,14 @@ export const renderCard = (movie, genres) => {
     <li class="movie-card">
       <article class="movie-card__article movie-item-js" data-id="${movie.id}">
         <img width="440" height="660" class="movie-card__img" src="${IMAGE_URL}${
-  movie.poster_path
-}">
+    movie.poster_path
+  }">
         <div class="movie-card__header">
           <h2 class="movie-card__title">${movie.title}</h2>
           <div class="movie-card__info">
-            ${currentGenres.join(', ')} | ${movie.release_date.substring(0, 4)}
+            ${currentGenres.join(', ')} | ${
+    movie.release_date ? `| ${movie.release_date.substring(0, 4)}` : ''
+  }
           </div>
         </div>
       </article>
