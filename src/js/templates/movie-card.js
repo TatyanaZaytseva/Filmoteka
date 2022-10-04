cc038d854240441a592da681896de8b1910daa9f
TaKosss <100071325+TaKosss@users.noreply.github.com>
2022-10-04 16:05:47 +0300

    erorr film poster add

--- src/js/templates/movie-card.js
+++ src/js/templates/movie-card.js
@@ -1,4 +1,5 @@
 import { IMAGE_URL } from '../globals';
+import film_poster from '../../images/film_poster.png'
 
 export const renderCard = (movie, genres) => {
   const currentGenres = movie.genre_ids.map(id => {
@@ -8,14 +9,13 @@ export const renderCard = (movie, genres) => {
   return `
     <li class="movie-card">
       <article class="movie-card__article movie-item-js" data-id="${movie.id}">
-        <img width="440" height="660" class="movie-card__img" src="${IMAGE_URL}${
-    movie.poster_path
-  }">
+        <img width="440" height="660" class="movie-card__img" src="${IMAGE_URL}${movie.poster_path}" 
+        onerror="this.src='${film_poster}'">
         <div class="movie-card__header">
           <h2 class="movie-card__title">${movie.title}</h2>
           <div class="movie-card__info">
-            ${currentGenres.join(', ')} | ${
-    movie.release_date ? ` ${movie.release_date.substring(0, 4)}` : ''
+            ${currentGenres.join(', ')} ${
+    movie.release_date ? `| ${movie.release_date.substring(0, 4)}` : ''
   }
           </div>
         </div>
